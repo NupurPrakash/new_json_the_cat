d0c77a08490518ce19cd8877b8634e9c1448610f
NupurPrakash <prakashnupurprakash@gmail.com>
2020-06-28 11:28:22 +0000

    Used request to get url of breeds, implemented dynamically and included condition breed not found

---
+++ breedFetcher.js
@@ -0,0 +1,23 @@
+const request = require('request');
+let breedName = process.argv[2];
+
+request(`https://api.thecatapi.com/v1/breeds/search?name=${breedName}`, (error, response, body) => {
+//request(`https://api.thecatapi.com/v1/breed/search?name=${breedName}`, (error, response, body) => {
+  if (error) {
+    console.log(`Error is, ${error}`);
+    console.log("StatusCode", response && response.statusCode);
+  } else {
+    //console.log(typeof body);
+    let data = JSON.parse(body);
+    //console.log(data);
+    //console.log("Type after parsing", typeof(data));
+    let breed = data[0];
+    if (breed) {
+      console.log("Description:", breed.description);
+    } else {
+      console.log("Breed not found")
+    }
+    
+  }
+
+})
